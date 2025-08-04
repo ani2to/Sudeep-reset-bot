4263c84c2abbf3bd56d1942458eafd26a47e0cca
ani2to <156508471+ani2to@users.noreply.github.com>
2025-08-04 21:49:15 +0530

    restbot👍🏼.py

--- restbot👍🏼.py
+++ restbot👍🏼.py
@@ -22,7 +22,7 @@ BACKUP_CHANNEL_LINK = "https://t.me/ScammerFuk"
 ADMIN_IDS = [6302016869]  # Replace with your admin user ID(s)
 DATABASE_NAME = "bot_users.db"
 
-bot = telebot.TeleBot(os.getenv("BOT_TOKEN") or "7558578299:AAGrufPgOT7DYq9_DMuIQFUClEd2CsBWBvA")
+bot = telebot.TeleBot(os.getenv("BOT_TOKEN") or "7558578299:AAENmVU8RysxMpEuNS7yxpQ_f03K7xumELQ")
 
 # Initialize database with enhanced tables
 def init_db():
